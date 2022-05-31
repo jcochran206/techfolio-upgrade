20967ff89b2f8b2d3d5ae8a4336aa9758bae6c36
jcochran206 <jonathan.o.cochran@gmail.com>
2022-05-30 21:18:43 -0700

    add react frontend to project and removed start files and added folder structure for project

---
+++ frontend_react/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className='app'>
+            Hello World!
+        </div>
+    );
+}
+
+export default App;
